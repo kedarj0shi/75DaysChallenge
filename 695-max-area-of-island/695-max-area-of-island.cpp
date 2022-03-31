b015bba2f54e2dd3b35e55e3ba6f4147db2c0f41
kedarj0shi <84830746+kedarj0shi@users.noreply.github.com>
2022-03-31 14:32:20 +0530

    Time: 18 ms (83.96%), Space: 23.7 MB (40.05%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    bool check(int i, int j, vector<vector<int>>& vis){
-        int n=vis.size(), m=vis[0].size();
-        if(i>=0 and j>=0 and i<n and j<m and !vis[i][j])
+    bool check(int i, int j,int n, int m){
+
+        if(i>=0 and j>=0 and i<n and j<m)
             return 1;
         return 0;
     }
-    int dfs(int x,int y,vector<vector<int>>& a, vector<vector<int>>& vis){
-        vis[x][y]=1;
+    int dfs(int x,int y,vector<vector<int>>& a){
+        a[x][y]=0;
         int dir[5]={0,1,0,-1,0};
+        int n=a.size(), m=a[0].size();
         int ans=1;
         for(int i=0;i<4;i++){
             int nx=x+dir[i], ny=y+dir[i+1];
-            if(check(nx,ny,vis) and a[nx][ny])
-                ans+=dfs(nx,ny,a,vis);
+            if(check(nx,ny,n,m) and a[nx][ny])
+                ans+=dfs(nx,ny,a);
         }
         return ans;
     }
     int maxAreaOfIsland(vector<vector<int>>& a) {
         int n=a.size(), m=a[0].size();
         
-        vector<vector<int>> vis(n, vector<int>(m,0));
+        // vector<vector<int>> vis(n, vector<int>(m,0));
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] and a[i][j]){
-                    int res=dfs(i,j,a,vis);
+                if(a[i][j]){
+                    int res=dfs(i,j,a);
                     ans=max(ans,res);
                 }
             }
